b402891b4a4c240f527bef7426bafdd20ac9ec61
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-08-23 21:47:38 +0330

    feat(article-172): compose article sections 2

---
+++ src/page/parts/Article172Part02.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Article172P02S01 } from "./Article172P02S01";
+import { Article172P02S02 } from "./Article172P02S02";
+import { Article172P02S03 } from "./Article172P02S03";
+
+export function Article172Part02() {
+  return (
+    <>
+      <Article172P02S01 />
+      <Article172P02S02 />
+      <Article172P02S03 />
+    </>
+  );
+}
