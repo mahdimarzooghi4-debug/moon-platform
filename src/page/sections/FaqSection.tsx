777d3b4afc70b17d6dec9f94a7ff54162a777704
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-08-23 22:22:03 +0330

    feat(contact): add faq section

---
+++ src/page/sections/FaqSection.tsx
@@ -0,0 +1,77 @@
+export function FaqSection() {
+  return (
+    <div className="flex pt-[60px] pr-[120px] pb-[60px] pl-[120px] flex-col gap-[32px] items-end self-stretch shrink-0 flex-nowrap bg-[#f4f6f8] relative z-[225]">
+      <div className="flex flex-col gap-[12px] items-center self-stretch shrink-0 flex-nowrap relative z-[226]">
+        <span className="flex w-[337px] h-[44px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[28px] font-extrabold leading-[43.75px] text-[#17324d] relative text-right whitespace-nowrap z-[227]">
+          شاید پاسخ شما همین‌جا باشد
+        </span>
+        <span className="flex w-[281px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-normal leading-[21.875px] text-[#60758a] relative text-right whitespace-nowrap z-[228]">
+          پرسش‌های متداول در حوزه‌های مختلف را مرور کنید
+        </span>
+      </div>
+      <div className="flex gap-[12px] justify-end items-start self-stretch shrink-0 flex-nowrap relative z-[229]">
+        <div className="flex w-[115px] pt-[8px] pr-[16px] pb-[8px] pl-[16px] items-start shrink-0 flex-nowrap bg-[#fff] rounded-[8px] border-solid border border-[#e4ebf1] relative z-[230]">
+          <span className="flex w-[83px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#60758a] relative text-right whitespace-nowrap z-[231]">
+            پرداخت و امنیت
+          </span>
+        </div>
+        <div className="flex w-[89px] pt-[8px] pr-[16px] pb-[8px] pl-[16px] items-start shrink-0 flex-nowrap bg-[#fff] rounded-[8px] border-solid border border-[#e4ebf1] relative z-[232]">
+          <span className="flex w-[57px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#60758a] relative text-right whitespace-nowrap z-[233]">
+            استارتاپ‌ها
+          </span>
+        </div>
+        <div className="flex w-[78px] pt-[8px] pr-[16px] pb-[8px] pl-[16px] items-start shrink-0 flex-nowrap bg-[#eaf5fd] rounded-[8px] border-solid border border-[#2094e3] relative z-[234]">
+          <span className="flex w-[46px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-bold leading-[20px] text-[#2094e3] relative text-right whitespace-nowrap z-[235]">
+            شرکت‌ها
+          </span>
+        </div>
+        <div className="flex w-[104px] pt-[8px] pr-[16px] pb-[8px] pl-[16px] items-start shrink-0 flex-nowrap bg-[#fff] rounded-[8px] border-solid border border-[#e4ebf1] relative z-[236]">
+          <span className="flex w-[72px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#60758a] relative text-right whitespace-nowrap z-[237]">
+            مشارکت فردی
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col gap-[12px] items-start self-stretch shrink-0 flex-nowrap relative z-[238]">
+        <div className="flex pt-[20px] pr-[20px] pb-[20px] pl-[20px] flex-col gap-[12px] items-start self-stretch shrink-0 flex-nowrap bg-[#fff] rounded-[12px] border-solid border border-[#e4ebf1] relative z-[239]">
+          <div className="flex justify-between items-center self-stretch shrink-0 flex-nowrap relative z-[240]">
+            <div className="w-[14px] h-[14px] shrink-0 bg-[url(/assets/codia/H6hkemACOy.png)] bg-cover bg-no-repeat relative overflow-hidden z-[241]" />
+            <span className="flex w-[334px] h-[23px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[15px] font-bold leading-[23px] text-[#17324d] relative text-right whitespace-nowrap z-[242]">
+              آیا واریزها در ماه مستقیم به حساب کمیته امداد می‌رود؟
+            </span>
+          </div>
+          <span className="h-[22px] self-stretch shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-normal leading-[22px] text-[#60758a] relative text-right whitespace-nowrap z-[243]">
+            بله، ۱۰۰٪ مبالغ مشارکت مستقیماً به حساب رسمی کمیته امداد امام
+            خمینی (ره) واریز می‌شود.
+          </span>
+        </div>
+        <div className="flex pt-[20px] pr-[20px] pb-[20px] pl-[20px] flex-col gap-[12px] items-start self-stretch shrink-0 flex-nowrap bg-[#fff] rounded-[12px] border-solid border border-[#e4ebf1] relative z-[244]">
+          <div className="flex justify-between items-center self-stretch shrink-0 flex-nowrap relative z-[245]">
+            <div className="w-[14px] h-[14px] shrink-0 bg-[url(/assets/codia/iXpzkuGWDe.png)] bg-cover bg-no-repeat relative overflow-hidden z-[246]" />
+            <span className="flex w-[325px] h-[23px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[15px] font-bold leading-[23px] text-[#17324d] relative text-right whitespace-nowrap z-[247]">
+              چگونه می‌توانم از تحقق واقعی KPI پروژه مطمئن شوم؟
+            </span>
+          </div>
+          <span className="h-[22px] self-stretch shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-normal leading-[22px] text-[#60758a] relative text-right whitespace-nowrap z-[248]">
+            تمام مستندات و خروجی‌های هر مرحله پس از بررسی کارشناسان خانه خلاق،
+            در پنل شما ثبت و به‌روزرسانی می‌شوند.
+          </span>
+        </div>
+        <div className="flex pt-[20px] pr-[20px] pb-[20px] pl-[20px] flex-col gap-[12px] items-start self-stretch shrink-0 flex-nowrap bg-[#fff] rounded-[12px] border-solid border border-[#e4ebf1] relative z-[249]">
+          <div className="flex justify-between items-center self-stretch shrink-0 flex-nowrap relative z-[250]">
+            <div className="w-[14px] h-[14px] shrink-0 bg-[url(/assets/codia/1cD1DZxvgn.png)] bg-cover bg-no-repeat relative overflow-hidden z-[251]" />
+            <span className="flex w-[406px] h-[23px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[15px] font-bold leading-[23px] text-[#17324d] relative text-right whitespace-nowrap z-[252]">
+              آیا شرکت ما پس از مشارکت گواهی موضوع ماده ۱۷۲ دریافت می‌کند؟
+            </span>
+          </div>
+          <span className="h-[22px] self-stretch shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-normal leading-[22px] text-[#60758a] relative text-right whitespace-nowrap z-[253]">
+            بله، در صورت تایید نهایی مراجع مالیاتی و تکمیل مدارک، گواهی مربوطه
+            در پنل سازمانی شما قرار می‌گیرد.
+          </span>
+        </div>
+      </div>
+      <span className="h-[22px] self-stretch shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-normal leading-[21.875px] text-[#2094e3] relative text-center underline whitespace-nowrap z-[254]">
+        مشاهده همه سؤالات متداول
+      </span>
+    </div>
+  );
+}
