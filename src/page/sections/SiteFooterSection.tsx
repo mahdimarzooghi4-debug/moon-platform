25d2ef16a2469770930c0be0668d0e14099ad4e4
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-08-23 22:22:43 +0330

    feat(contact): add footer section

---
+++ src/page/sections/SiteFooterSection.tsx
@@ -0,0 +1,87 @@
+export function SiteFooterSection() {
+  return (
+    <div className="flex pt-[80px] pr-[120px] pb-[40px] pl-[120px] flex-col gap-[40px] items-end self-stretch shrink-0 flex-nowrap bg-[#111e30] relative z-[274]">
+      <div className="flex gap-[48px] justify-end items-start self-stretch shrink-0 flex-nowrap relative z-[275]">
+        <div className="flex w-[220px] h-[182px] flex-col gap-[12px] items-end shrink-0 flex-nowrap relative z-[276]">
+          <span className="flex w-[59px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-bold leading-[21.875px] text-[#fff] relative text-right whitespace-nowrap z-[277]">
+            ارتباط با ما
+          </span>
+          <span className="flex w-[115px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[278]">
+            تلفن: ۰۲۱-۸۸۸۸۸۸۸۸
+          </span>
+          <span className="flex w-[156px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[279]">
+            ایمیل: info@mahplatform.ir
+          </span>
+          <div className="flex w-[48px] pt-[8px] pr-0 pb-0 pl-0 gap-[12px] items-center shrink-0 flex-nowrap relative z-[280]">
+            <div className="flex w-[18px] h-[18px] flex-col justify-center items-center shrink-0 flex-nowrap relative overflow-hidden z-[281]">
+              <div className="w-[18px] h-[18px] shrink-0 bg-[url(/assets/codia/AqYBh3U1n5.png)] bg-cover bg-no-repeat relative overflow-hidden z-[282]" />
+            </div>
+            <div className="flex w-[18px] h-[18px] flex-col justify-center items-center shrink-0 flex-nowrap relative overflow-hidden z-[283]">
+              <div className="w-[18px] h-[18px] shrink-0 bg-[url(/assets/codia/T8QnShg0C5.png)] bg-cover bg-no-repeat relative overflow-hidden z-[284]" />
+            </div>
+          </div>
+        </div>
+        <div className="flex w-[150px] h-[182px] flex-col gap-[12px] items-end shrink-0 flex-nowrap relative z-[285]">
+          <span className="flex w-[39px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-bold leading-[21.875px] text-[#fff] relative text-right whitespace-nowrap z-[286]">
+            حقوقی
+          </span>
+          <span className="flex w-[82px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[287]">
+            قوانین و مقررات
+          </span>
+          <span className="flex w-[75px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[288]">
+            حریم خصوصی
+          </span>
+        </div>
+        <div className="flex w-[160px] h-[182px] flex-col gap-[12px] items-end shrink-0 flex-nowrap relative z-[289]">
+          <span className="flex w-[49px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-bold leading-[21.875px] text-[#fff] relative text-right whitespace-nowrap z-[290]">
+            پشتیبانی
+          </span>
+          <span className="flex w-[55px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[291]">
+            تماس با ما
+          </span>
+          <span className="flex w-[77px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[292]">
+            سؤالات متداول
+          </span>
+          <span className="flex w-[82px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[293]">
+            پیگیری مشارکت
+          </span>
+        </div>
+        <div className="flex w-[180px] flex-col gap-[12px] items-end shrink-0 flex-nowrap relative z-[294]">
+          <span className="flex w-[83px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-bold leading-[21.875px] text-[#fff] relative text-right whitespace-nowrap z-[295]">
+            دسترسی سریع
+          </span>
+          <span className="flex w-[38px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[296]">
+            پروژه‌ها
+          </span>
+          <span className="flex w-[69px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[297]">
+            برای شرکت‌ها
+          </span>
+          <span className="flex w-[71px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[298]">
+            نحوه مشارکت
+          </span>
+          <span className="flex w-[84px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[299]">
+            ثبت‌نام استارتاپ
+          </span>
+          <span className="flex w-[80px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#e4ebf1] relative text-right whitespace-nowrap z-[300]">
+            اخبار و رویدادها
+          </span>
+        </div>
+        <div className="flex w-[298px] h-[182px] flex-col gap-[14px] items-end shrink-0 flex-nowrap relative z-[301]">
+          <div className="w-[210px] h-[70px] shrink-0 bg-[url(/assets/codia/8JOAgRR3qW.png)] bg-cover bg-no-repeat relative z-[302]" />
+          <span className="flex w-[298px] h-[44px] justify-end items-start self-stretch shrink-0 font-['Vazirmatn'] text-[13px] font-normal leading-[22px] text-[#e4ebf1] relative text-right z-[303]">
+            سامانه مشارکت در پروژه‌های اثر اجتماعی با مسیر مالی شفاف و اثر
+            قابل‌اندازه‌گیری.
+          </span>
+        </div>
+      </div>
+      <div className="flex pt-[20px] pr-0 pb-0 pl-0 flex-col gap-[4px] items-center self-stretch shrink-0 flex-nowrap border-solid border-b border-b-[rgba(255,255,255,0.12)] relative z-[304]">
+        <span className="flex w-[219px] h-[19px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[18.75px] text-[#e4ebf1] relative text-center whitespace-nowrap z-[305]">
+          طراحی و توسعه با هدف ارتقای عدالت اجتماعی
+        </span>
+        <span className="flex w-[203px] h-[19px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[18.75px] text-[#e4ebf1] relative text-center whitespace-nowrap z-[306]">
+          تمامی حقوق برای سامانه ماه محفوظ است.
+        </span>
+      </div>
+    </div>
+  );
+}
