1fa0ad3231f27d0c7544125923972ad3b91229b3
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-08-25 17:56:05 +0330

    feat: add completed OTP verification page

---
+++ src/page/index.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container flex w-[1440px] flex-col items-center flex-nowrap bg-[#f7fafc] relative mx-auto my-0">
+      <div className="flex h-[80px] pt-0 pr-[120px] pb-0 pl-[120px] justify-between items-center self-stretch shrink-0 flex-nowrap bg-[#fff] border-solid border-t border-t-[#e8e8e8] relative">
+        <div className="flex w-[150px] gap-[8px] items-center shrink-0 flex-nowrap relative z-[1]">
+          <div className="flex w-[16px] h-[16px] flex-col justify-center items-center shrink-0 flex-nowrap relative overflow-hidden z-[2]">
+            <div className="w-[16px] h-[16px] shrink-0 bg-[url(/assets/1M6r021j9U.png)] bg-cover bg-no-repeat relative overflow-hidden z-[3]" />
+          </div>
+          <span className="flex w-[126px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-medium leading-[21.875px] text-[#4a5568] relative text-right whitespace-nowrap z-[4]">
+            بازگشت به صفحه ورود
+          </span>
+        </div>
+        <div className="flex w-[137px] gap-[12px] items-center shrink-0 flex-nowrap relative z-[5]">
+          <div className="w-[137px] h-[46px] shrink-0 bg-[url(/assets/YccaNps5AJ.png)] bg-cover bg-no-repeat relative z-[6]" />
+        </div>
+      </div>
+      <div className="flex pt-[48px] pr-0 pb-[48px] pl-0 flex-col items-center self-stretch shrink-0 flex-nowrap relative z-[7]">
+        <div className="flex w-[540px] pt-[36px] pr-[36px] pb-[36px] pl-[36px] flex-col gap-[24px] items-center shrink-0 flex-nowrap bg-[#fff] rounded-[20px] border-solid border border-[#e4ebf1] relative shadow-[0_4px_20px_0_rgba(0,0,0,0.04)] z-[8]">
+          <div className="flex w-[64px] h-[64px] justify-center items-center shrink-0 flex-nowrap bg-[#eaf5fd] rounded-[32px] relative z-[9]">
+            <div className="w-[28px] h-[28px] shrink-0 bg-[url(/assets/r3LkYver1w.png)] bg-cover bg-no-repeat relative overflow-hidden z-10" />
+          </div>
+          <div className="flex flex-col gap-[8px] items-center self-stretch shrink-0 flex-nowrap relative z-[11]">
+            <span className="flex w-[184px] h-[38px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[24px] font-bold leading-[37.5px] text-[#1a202c] relative text-center whitespace-nowrap z-[12]">
+              کد تأیید را وارد کنید
+            </span>
+            <span className="flex w-[260px] h-[22px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-normal leading-[21.875px] text-[#718096] relative text-center whitespace-nowrap z-[13]">
+              کد پنج‌رقمی ارسال‌شده به شماره زیر را وارد کنید.
+            </span>
+            <div className="flex w-[164px] gap-[8px] justify-center items-center shrink-0 flex-nowrap relative z-[14]">
+              <span className="flex w-[79px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-semibold leading-[21.875px] text-[#2094e3] relative text-right whitespace-nowrap z-[15]">
+                ویرایش شماره
+              </span>
+              <span className="flex w-[77px] h-[23px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[15px] font-medium leading-[23px] text-[#1a202c] relative text-right whitespace-nowrap z-[16]">
+                ۰۹۱۲•••۴۵۶۷
+              </span>
+            </div>
+          </div>
+          <div className="flex gap-[12px] justify-center items-start self-stretch shrink-0 flex-nowrap relative z-[17]">
+            <div className="flex w-[52px] h-[56px] justify-center items-center shrink-0 flex-nowrap bg-[#fff] rounded-[12px] border-solid border border-[#a0aec0] relative z-[18]">
+              <span className="flex w-[13px] h-[31px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[20px] font-bold leading-[31px] text-[#1a202c] relative text-center whitespace-nowrap z-[19]">
+                ۸
+              </span>
+            </div>
+            <div className="flex w-[52px] h-[56px] justify-center items-center shrink-0 flex-nowrap bg-[#fff] rounded-[12px] border-solid border border-[#a0aec0] relative z-20">
+              <span className="flex w-[11px] h-[31px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[20px] font-bold leading-[31px] text-[#1a202c] relative text-center whitespace-nowrap z-[21]">
+                ۲
+              </span>
+            </div>
+            <div className="flex w-[52px] h-[56px] justify-center items-center shrink-0 flex-nowrap bg-[#fff] rounded-[12px] border-solid border border-[#a0aec0] relative z-[22]">
+              <span className="flex w-[13px] h-[31px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[20px] font-bold leading-[31px] text-[#1a202c] relative text-center whitespace-nowrap z-[23]">
+                ۷
+              </span>
+            </div>
+            <div className="flex w-[52px] h-[56px] justify-center items-center shrink-0 flex-nowrap bg-[#fff] rounded-[12px] border-solid border border-[#a0aec0] relative z-[24]">
+              <span className="flex w-[14px] h-[31px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[20px] font-bold leading-[31px] text-[#1a202c] relative text-center whitespace-nowrap z-[25]">
+                ۳
+              </span>
+            </div>
+            <div className="flex w-[52px] h-[56px] justify-center items-center shrink-0 flex-nowrap bg-[#fff] rounded-[12px] border-solid border border-[#a0aec0] relative z-[26]">
+              <span className="flex w-[13px] h-[31px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[20px] font-bold leading-[31px] text-[#1a202c] relative text-center whitespace-nowrap z-[27]">
+                ۵
+              </span>
+            </div>
+          </div>
+          <span className="flex w-[110px] h-[20px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#718096] relative text-center whitespace-nowrap z-[28]">
+            ارسال مجدد کد تا ۱:۰۰
+          </span>
+          <div className="flex pt-[14px] pr-[32px] pb-[14px] pl-[32px] justify-center items-center self-stretch shrink-0 flex-nowrap rounded-[12px] relative shadow-[0_4px_12px_0_rgba(32,148,227,0.25)] z-[29]">
+            <span className="h-[25px] shrink-0 basis-auto font-['Vazirmatn'] text-[16px] font-semibold leading-[25px] text-[#fff] relative text-left whitespace-nowrap z-30">
+              تأیید و ورود
+            </span>
+          </div>
+          <div className="flex pt-[16px] pr-0 pb-0 pl-0 flex-col gap-[8px] items-center self-stretch shrink-0 flex-nowrap relative z-[31]">
+            <div className="flex w-[283px] gap-[6px] items-center shrink-0 flex-nowrap relative z-[32]">
+              <span className="flex w-[263px] h-[19px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[18.75px] text-[#718096] relative text-center whitespace-nowrap z-[33]">
+                کد تأیید محرمانه است؛ آن را در اختیار دیگران قرار ندهید.
+              </span>
+              <div className="w-[14px] h-[14px] shrink-0 bg-[url(/assets/Cu2QHWVDxE.png)] bg-cover bg-no-repeat relative overflow-hidden z-[34]" />
+            </div>
+            <span className="flex w-[250px] h-[17px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[11px] font-normal leading-[17px] text-[#a0aec0] relative text-center whitespace-nowrap z-[35]">
+              ورود شما با کد یک‌بارمصرف و بدون رمز عبور انجام می‌شود.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="flex pt-[24px] pr-0 pb-[40px] pl-0 flex-col gap-[12px] items-center self-stretch shrink-0 flex-nowrap relative z-[36]">
+        <span className="flex w-[347px] h-[19px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[18.75px] text-[#a0aec0] relative text-center whitespace-nowrap z-[37]">
+          اطلاعات شما مطابق سیاست حریم خصوصی سامانه ماه محافظت می‌شود.
+        </span>
+        <div className="flex w-[180px] gap-[16px] items-center shrink-0 flex-nowrap relative z-[38]">
+          <span className="flex w-[76px] h-[19px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[18.75px] text-[#718096] relative text-right whitespace-nowrap z-[39]">
+            قوانین و مقررات
+          </span>
+          <span className="h-[19px] shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[18.75px] text-[#93a3b7] relative text-left whitespace-nowrap z-40">
+            |
+          </span>
+          <span className="flex w-[69px] h-[19px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[18.75px] text-[#718096] relative text-right whitespace-nowrap z-[41]">
+            حریم خصوصی
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
