cc84bec038d123447cd7fa5a4f1714af110314b8
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-08-28 09:54:04 +0330

    feat: add CMS login attempt limit state

---
+++ src/page/index.tsx
@@ -0,0 +1,132 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container flex w-[1440px] h-[900px] items-start flex-nowrap bg-[#f7fafc] relative overflow-hidden mx-auto my-0">
+      <div className="flex w-[648px] pt-[48px] pr-[48px] pb-[48px] pl-[48px] flex-col justify-center items-center self-stretch shrink-0 flex-nowrap bg-[#f7fafc] relative">
+        <div className="flex flex-col gap-[28px] items-start self-stretch shrink-0 flex-nowrap relative z-[1]">
+          <div className="flex pt-[36px] pr-[36px] pb-[36px] pl-[36px] flex-col gap-[24px] items-start self-stretch shrink-0 flex-nowrap bg-[#fff] rounded-[24px] border-solid border border-[#e4ebf1] relative shadow-[0_4px_16px_0_rgba(22,45,71,0.08)] z-[2]">
+            <div className="flex flex-col gap-[16px] items-end self-stretch shrink-0 flex-nowrap relative z-[3]">
+              <div className="flex w-[88px] pt-[6px] pr-[12px] pb-[6px] pl-[12px] justify-center items-center shrink-0 flex-nowrap bg-[#fef0ee] rounded-[12px] relative z-[4]">
+                <span className="flex w-[64px] h-[20px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[20px] text-[#d92d20] relative text-center whitespace-nowrap z-[5]">
+                  امنیت حساب
+                </span>
+              </div>
+              <span className="h-[34px] self-stretch shrink-0 basis-auto font-['Vazirmatn'] text-[22px] font-bold leading-[34px] text-[#17324d] relative text-right whitespace-nowrap z-[6]">
+                تعداد تلاش‌های ناموفق بیش از حد مجاز
+              </span>
+              <span className="flex w-[408px] h-[44px] justify-end items-start self-stretch shrink-0 font-['Vazirmatn'] text-[14px] font-normal leading-[22px] text-[#60758a] relative text-right z-[7]">
+                برای حفظ امنیت حساب، ورود موقتاً محدود شده است. پس از زمان
+                اعلام‌شده یا از طریق بازیابی حساب دوباره تلاش کنید.
+              </span>
+            </div>
+            <div className="flex pt-[16px] pr-[16px] pb-[16px] pl-[16px] gap-[12px] items-center self-stretch shrink-0 flex-nowrap bg-[#fef0ee] rounded-[12px] relative z-[8]">
+              <div className="flex flex-col gap-[4px] items-end grow shrink-0 basis-0 flex-nowrap relative z-[9]">
+                <span className="h-[22px] self-stretch shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-bold leading-[21.875px] text-[#d92d20] relative text-right whitespace-nowrap z-10">
+                  تلاش مجدد پس از: ۱۴:۵۹ دقیقه
+                </span>
+              </div>
+              <div className="w-[20px] h-[20px] shrink-0 bg-[url(/assets/codia/EgeeYe3NLr.svg)] bg-cover bg-no-repeat relative overflow-hidden z-[11]" />
+            </div>
+            <div className="flex flex-col gap-[12px] items-start self-stretch shrink-0 flex-nowrap relative z-[12]">
+              <div className="flex pt-[11px] pr-[20px] pb-[11px] pl-[20px] gap-[8px] justify-center items-center self-stretch shrink-0 flex-nowrap bg-[#2094e3] rounded-[12px] relative z-[13]">
+                <span className="flex w-[77px] h-[22px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-medium leading-[22px] text-[#fff] relative text-center whitespace-nowrap z-[14]">
+                  بازیابی حساب
+                </span>
+              </div>
+              <div className="flex pt-[12px] pr-[12px] pb-[12px] pl-[12px] justify-center items-start self-stretch shrink-0 flex-nowrap relative z-[15]">
+                <span className="h-[22px] shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-semibold leading-[21.875px] text-[#60758a] relative text-left whitespace-nowrap z-[16]">
+                  بازگشت به وب‌سایت
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[12px] items-center self-stretch shrink-0 flex-nowrap relative z-[17]">
+            <div className="flex w-[335px] gap-[16px] justify-center items-start shrink-0 flex-nowrap relative z-[18]">
+              <span className="flex w-[75px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#60758a] relative text-right whitespace-nowrap z-[19]">
+                حریم خصوصی
+              </span>
+              <span className="h-[20px] shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#60758a] relative text-left whitespace-nowrap z-20">
+                |
+              </span>
+              <span className="flex w-[75px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#60758a] relative text-right whitespace-nowrap z-[21]">
+                شرایط استفاده
+              </span>
+              <span className="h-[20px] shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#60758a] relative text-left whitespace-nowrap z-[22]">
+                |
+              </span>
+              <span className="flex w-[113px] h-[20px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[13px] font-normal leading-[20px] text-[#60758a] relative text-right whitespace-nowrap z-[23]">
+                راهنمای امنیت حساب
+              </span>
+            </div>
+            <span className="flex w-[224px] h-[17px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[11px] font-normal leading-[17px] text-[#60758a] relative text-right whitespace-nowrap z-[24]">
+              © سامانه ماه · مدیریت مسئولیت اجتماعی هوشمند
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-[792px] pt-[64px] pr-[64px] pb-[64px] pl-[64px] flex-col justify-between items-start self-stretch shrink-0 flex-nowrap bg-[#eaf5fd] border-solid border-l border-l-[#e4ebf1] relative z-[25]">
+        <div className="flex gap-[12px] justify-end items-center self-stretch shrink-0 flex-nowrap relative z-[26]">
+          <img
+            className="w-[137px] h-[46px] shrink-0 object-cover relative z-[27]"
+            src="/assets/codia/FJ51wjjmr4.png"
+            alt="سامانه ماه"
+          />
+        </div>
+        <div className="flex flex-col gap-[32px] items-end self-stretch shrink-0 flex-nowrap relative z-[28]">
+          <div className="flex flex-col gap-[12px] items-end self-stretch shrink-0 flex-nowrap relative z-[29]">
+            <div className="flex w-[136px] justify-end items-start shrink-0 flex-nowrap relative z-30">
+              <div className="flex w-[136px] pt-[6px] pr-[12px] pb-[6px] pl-[12px] justify-center items-center shrink-0 flex-nowrap bg-[#eaf5fd] rounded-[12px] relative z-[31]">
+                <span className="flex w-[112px] h-[20px] justify-center items-start shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[20px] text-[#2094e3] relative text-center whitespace-nowrap z-[32]">
+                  مدیریت محتوای عمومی
+                </span>
+              </div>
+            </div>
+            <span className="h-[50px] self-stretch shrink-0 basis-auto font-['Vazirmatn'] text-[32px] font-bold leading-[50px] text-[#17324d] relative text-right whitespace-nowrap z-[33]">
+              مدیریت شفاف محتوای سامانه ماه
+            </span>
+            <span className="flex w-[664px] h-[52px] justify-end items-start self-stretch shrink-0 font-['Vazirmatn'] text-[15px] font-normal leading-[26px] text-[#60758a] relative text-right z-[34]">
+              این بخش فقط در اختیار اعضای تأییدشده تیم محتوا، بازبین‌ها و ناشران
+              سامانه قرار دارد. دسترسی به پنل CMS مشمول سیاست‌ها و راهنمای امنیت
+              حساب است.
+            </span>
+          </div>
+          <div className="flex flex-col gap-[16px] items-start self-stretch shrink-0 flex-nowrap relative z-[35]">
+            <div className="flex gap-[12px] justify-end items-center self-stretch shrink-0 flex-nowrap relative z-[36]">
+              <span className="flex w-[131px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-medium leading-[21.875px] text-[#17324d] relative text-right whitespace-nowrap z-[37]">
+                مدیریت محتوای عمومی
+              </span>
+              <div className="flex w-[32px] h-[32px] justify-center items-center shrink-0 flex-nowrap bg-[#fff] rounded-[8px] border-solid border border-[#e4ebf1] relative z-[38]">
+                <div className="w-[16px] h-[16px] shrink-0 bg-[url(/assets/codia/A3d2TmdmP4.svg)] bg-cover bg-no-repeat relative overflow-hidden z-[39]" />
+              </div>
+            </div>
+            <div className="flex gap-[12px] justify-end items-center self-stretch shrink-0 flex-nowrap relative z-40">
+              <span className="flex w-[142px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-medium leading-[21.875px] text-[#17324d] relative text-right whitespace-nowrap z-[41]">
+                بازبینی و انتشار کنترل‌شده
+              </span>
+              <div className="flex w-[32px] h-[32px] justify-center items-center shrink-0 flex-nowrap bg-[#fff] rounded-[8px] border-solid border border-[#e4ebf1] relative z-[42]">
+                <div className="w-[16px] h-[16px] shrink-0 bg-[url(/assets/codia/8hcyy7uuRH.svg)] bg-cover bg-no-repeat relative overflow-hidden z-[43]" />
+              </div>
+            </div>
+            <div className="flex gap-[12px] justify-end items-center self-stretch shrink-0 flex-nowrap relative z-[44]">
+              <span className="flex w-[118px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-medium leading-[21.875px] text-[#17324d] relative text-right whitespace-nowrap z-[45]">
+                ثبت و ردیابی تغییرات
+              </span>
+              <div className="flex w-[32px] h-[32px] justify-center items-center shrink-0 flex-nowrap bg-[#fff] rounded-[8px] border-solid border border-[#e4ebf1] relative z-[46]">
+                <div className="w-[16px] h-[16px] shrink-0 bg-[url(/assets/codia/NrEFARB0HG.svg)] bg-cover bg-no-repeat relative overflow-hidden z-[47]" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-[8px] justify-end items-center self-stretch shrink-0 flex-nowrap relative z-[48]">
+          <span className="h-[19px] grow shrink-0 basis-auto font-['Vazirmatn'] text-[12px] font-normal leading-[18.75px] text-[#60758a] relative text-right whitespace-nowrap z-[49]">
+            دسترسی به این بخش براساس نقش سازمانی و مجوزهای تأییدشده انجام
+            می‌شود.
+          </span>
+          <div className="w-[14px] h-[14px] shrink-0 bg-[url(/assets/codia/aJGU6SPE4A.svg)] bg-cover bg-no-repeat relative overflow-hidden z-50" />
+        </div>
+      </div>
+    </div>
+  );
+}
