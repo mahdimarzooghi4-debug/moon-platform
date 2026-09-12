3a7b61ce7328d1e17775b69cc95d19f6e6c74633
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-13 02:06:05 +0330

    feat(how-it-works): mount page interactions

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AuthGate } from "./auth/AuthGate";
 import PublicFooter from "./shared/PublicFooter";
 import StartupTeamMemberAddPage from "./features/startup-registration/states/step2/team-member";
 import ProjectImpactDocumentsPage from "./project-impact-report/documents";
+import HowItWorksInteractions from "./how-it-works/HowItWorksInteractions";
 
 const STARTUP_TEAM_MEMBER_PATH = "/register/startup/states/step2/team-member/new";
 const PROJECT_IMPACT_DOCUMENTS_PATH = "/projects/impact-report/documents";
@@ -15,6 +16,7 @@ export default function App() {
   return (
     <AppProviders>
       <AuthGate>
+        <HowItWorksInteractions />
         {isStartupTeamMemberPage ? (
           <StartupTeamMemberAddPage />
         ) : isProjectImpactDocumentsPage ? (
