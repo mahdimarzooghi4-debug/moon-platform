35ec10f3089878dac658e57a6cd56581886727a1
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-12 22:36:06 +0330

    load startup registration step2 interactions

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import App from "./App";
 import "./landing/contact-details.css";
 import "./landing/interactions";
 import "./startups/interactions";
+import "./features/startup-registration/states/step2/interactions";
 
 document.documentElement.lang = "fa";
 document.documentElement.dir = "rtl";
