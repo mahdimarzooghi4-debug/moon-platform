3cb673c2f6af89446fead09f2f991a413f549cdd
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-12 22:46:43 +0330

    feat(startup-registration): expose add team member page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { AppProviders } from "./app/AppProviders";
 import { AppRouter } from "./app/router";
 import { AuthGate } from "./auth/AuthGate";
 import PublicFooter from "./shared/PublicFooter";
+import StartupTeamMemberAddPage from "./features/startup-registration/states/step2/team-member";
+
+const STARTUP_TEAM_MEMBER_PATH = "/register/startup/states/step2/team-member/new";
 
 export default function App() {
+  const isStartupTeamMemberPage = window.location.pathname === STARTUP_TEAM_MEMBER_PATH;
+
   return (
     <AppProviders>
       <AuthGate>
-        <AppRouter />
+        {isStartupTeamMemberPage ? <StartupTeamMemberAddPage /> : <AppRouter />}
         <PublicFooter />
       </AuthGate>
     </AppProviders>
