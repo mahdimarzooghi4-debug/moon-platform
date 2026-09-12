7071d354fdeb5eaae8add14d7d085b3f9a1a6c34
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-13 01:13:10 +0330

    feat(impact-report): route documents viewer

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,25 @@ import { AppRouter } from "./app/router";
 import { AuthGate } from "./auth/AuthGate";
 import PublicFooter from "./shared/PublicFooter";
 import StartupTeamMemberAddPage from "./features/startup-registration/states/step2/team-member";
+import ProjectImpactDocumentsPage from "./project-impact-report/documents";
 
 const STARTUP_TEAM_MEMBER_PATH = "/register/startup/states/step2/team-member/new";
+const PROJECT_IMPACT_DOCUMENTS_PATH = "/projects/impact-report/documents";
 
 export default function App() {
   const isStartupTeamMemberPage = window.location.pathname === STARTUP_TEAM_MEMBER_PATH;
+  const isProjectImpactDocumentsPage = window.location.pathname === PROJECT_IMPACT_DOCUMENTS_PATH;
 
   return (
     <AppProviders>
       <AuthGate>
-        {isStartupTeamMemberPage ? <StartupTeamMemberAddPage /> : <AppRouter />}
+        {isStartupTeamMemberPage ? (
+          <StartupTeamMemberAddPage />
+        ) : isProjectImpactDocumentsPage ? (
+          <ProjectImpactDocumentsPage />
+        ) : (
+          <AppRouter />
+        )}
         <PublicFooter />
       </AuthGate>
     </AppProviders>
