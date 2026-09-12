354899aa3510ad1e61658d9af461ac90206a0812
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-13 00:42:46 +0330

    feat(contact): show success state after support submit

--- src/contact-support/index.tsx
+++ src/contact-support/index.tsx
@@ -7,8 +7,15 @@ import { QuickLinksSection } from "./sections/QuickLinksSection";
 import { ContactAndRequestSection } from "./sections/ContactAndRequestSection";
 import { FaqSection } from "./sections/FaqSection";
 import { TrackingActionsSection } from "./sections/TrackingActionsSection";
+import SupportSuccessPage from "./SupportSuccessPage";
 
 export default function Main() {
+  const trackingCode = new URLSearchParams(window.location.search).get("submitted");
+
+  if (trackingCode) {
+    return <SupportSuccessPage trackingCode={trackingCode} />;
+  }
+
   return (
     <div className="main-container flex w-[1440px] flex-col items-center flex-nowrap bg-[#fcfbf8] relative overflow-hidden mx-auto my-0">
       <MainNavigationSection />
