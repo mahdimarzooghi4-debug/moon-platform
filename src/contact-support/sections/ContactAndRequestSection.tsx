895b7e90f2a6ae56380c68c0f65b62901e14f84d
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-25 16:30:16 +0330

    deploy: port about and contact RTL src/contact-support/sections/ContactAndRequestSection.tsx

--- src/contact-support/sections/ContactAndRequestSection.tsx
+++ src/contact-support/sections/ContactAndRequestSection.tsx
@@ -3,7 +3,7 @@ import { SupportRequestForm } from "./SupportRequestForm";
 
 export function ContactAndRequestSection() {
   return (
-    <div className="flex pt-[20px] pr-[120px] pb-[40px] pl-[120px] gap-[40px] justify-end items-start self-stretch shrink-0 flex-nowrap relative z-[79]">
+    <div className="mah-contact-details flex pt-[20px] pr-[120px] pb-[40px] pl-[120px] gap-[40px] justify-end items-start self-stretch shrink-0 flex-nowrap relative z-[79]">
       <ContactInfoColumn />
       <SupportRequestForm />
     </div>
