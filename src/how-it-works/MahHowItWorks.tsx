e11b8c5b024cdfbaf7ca09ced0ada56cc015cfa1
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-25 13:34:50 +0330

    deploy: polish public copy and presentation

--- src/how-it-works/MahHowItWorks.tsx
+++ src/how-it-works/MahHowItWorks.tsx
@@ -60,7 +60,7 @@ export default function MahHowItWorks() {
               <div className="flex w-[318px] gap-[32px] items-center shrink-0 flex-nowrap relative z-[23]">
                 <div className="flex w-[110px] pt-[16px] pr-[16px] pb-[16px] pl-[16px] justify-center items-center shrink-0 flex-nowrap bg-[#eaf5fd] rounded-[16px] border-solid border-2 border-[#2094e3] relative z-[24]">
                   <span className="flex w-[78px] h-[22px] justify-end items-start shrink-0 basis-auto font-['Vazirmatn'] text-[14px] font-bold leading-[21.875px] text-[#2094e3] relative text-right whitespace-nowrap z-[25]">
-                    پروژه اجتماعی
+                    پروژه دارای اثر اجتماعی
                   </span>
                 </div>
                 <div className="w-[40px] h-[2px] shrink-0 bg-[url(/assets/codia/9Sbh3zvpwd.png)] bg-cover bg-no-repeat relative overflow-hidden z-[26]" />
@@ -1613,7 +1613,7 @@ export default function MahHowItWorks() {
           <div className="flex w-[298px] h-[182px] flex-col gap-[14px] items-end shrink-0 flex-nowrap relative z-[692]">
             <div className="w-[210px] h-[70px] shrink-0 bg-[url(/assets/codia/61LMmHHrqJ.png)] bg-cover bg-no-repeat relative z-[693]" />
             <span className="flex w-[298px] h-[44px] justify-end items-start self-stretch shrink-0 font-['Vazirmatn'] text-[13px] font-normal leading-[22px] text-[#e4ebf1] relative text-right z-[694]">
-              سامانه مشارکت در پروژه‌های اثر اجتماعی با مسیر مالی شفاف و اثر
+              سامانه مشارکت در پروژه‌های دارای اثر اجتماعی با مسیر مالی شفاف و اثر
               قابل‌اندازه‌گیری.
             </span>
           </div>
